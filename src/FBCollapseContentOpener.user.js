38aecc19aedb258934a2db12f135ecf5b748cc41
Haoger <haoger1012@gmail.com>
2023-11-23 22:29:20 +0800

    Facebook 展開留言 v0.2

--- src/FBCollapseContentOpener.user.js
+++ src/FBCollapseContentOpener.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FBCollapseContentOpener
 // @namespace    https://github.com/haoger1012/TampermonkeyUserscripts/raw/main/src/FBCollapseContentOpener.user.js
-// @version      0.1
+// @version      0.2
 // @description  open all collapse content in visible range of FB
 // @author       Haoger
 // @match        https://www.facebook.com/*
@@ -15,22 +15,21 @@
     document.addEventListener('keydown', (ev) => {
         if (ev.altKey && ev.key === 'a' && !/^(?:input|select|textarea|button)$/i.test(ev.target.nodeName)) {
             (function () {
-                [...document.querySelectorAll("span[dir='auto']")]
-                    .filter(span => {
-                        const rect = span.getBoundingClientRect()
-                        return rect.top > 0 && rect.top < window.innerHeight
-                    })
+                [...document.querySelectorAll("div[role=dialog] span[dir='auto']")]
+                    .filter(getViewport)
                     .filter(span => span.textContent.match(/\d+\s?(?:則|個).*(?:回覆|回答)/) || span.textContent.match(/(?:查看|檢視|顯示)(?:.*\d+\s?(?:則|個)|).*(留言|答案)/))
                     .forEach(span => span.click());
 
                 [...document.querySelectorAll("div[role=main] div[role='button']")]
-                    .filter(div => {
-                        const rect = div.getBoundingClientRect()
-                        return rect.top > 0 && rect.top < window.innerHeight
-                    })
-                    .filter(div => div.textContent === '顯示更多')
+                    .filter(getViewport)
+                    .filter(div => div.textContent.endsWith("更多"))
                     .forEach(div => div.click());
             })();
         }
     })
+
+    function getViewport(div) {
+        const rect = div.getBoundingClientRect()
+        return rect.top > 0 && rect.top < window.innerHeight
+    }
 })();
