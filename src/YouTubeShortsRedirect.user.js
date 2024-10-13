b0e527fe852fa98b6d68fbee7c33e9a77efe2ac8
Haoger <haoger1012@gmail.com>
2024-10-13 22:15:06 +0800

    YouTube Shorts 跳轉 v0.2

--- src/YouTubeShortsRedirect.user.js
+++ src/YouTubeShortsRedirect.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         YouTubeShortsRedirect
+// @name         YouTube Shorts Redirect
 // @namespace    https://github.com/haoger1012/TampermonkeyUserscripts/raw/main/src/YouTubeShortsRedirect.user.js
-// @version      0.1
+// @version      0.2
 // @description  Redirect YouTube Shorts to common video page
 // @author       Haoger
 // @match        https://www.youtube.com/*
@@ -12,32 +12,46 @@
 
 (function () {
     'use strict';
-    var previousHref = location.href;
 
-    window.addEventListener("load", function () {
-        const observer = new MutationObserver(function (mutations) {
-            mutations.forEach(function () {
-                if (previousHref != location.href) {
-                    previousHref = location.href;
-                    redirect();
-                }
-            });
-        });
+    const oldReplaceState = history.replaceState;
+    history.replaceState = function replaceState() {
+        let ret = oldReplaceState.apply(this, arguments);
+        window.dispatchEvent(new Event("replacestate"));
+        window.dispatchEvent(new Event("locationchange"));
+        return ret;
+    };
 
-        const target = document.querySelector("body");
-
-        const options = {
-            childList: true,
-            subtree: true
-        };
-
-        observer.observe(target, options);
+    window.addEventListener("popstate", () => {
+        window.dispatchEvent(new Event("locationchange"));
     });
 
-    function redirect() {
-        const ln = location.href;
-        if (ln.indexOf("www.youtube.com/shorts") >= 0) {
-            void (location.href = ln.replace(/www\.youtube\.com\/shorts/i, "youtu.be"));
+    window.addEventListener("transitionend", (e) => {
+        if (e.target.id === "progress") {
+            window.dispatchEvent(new Event("locationchange"));
+        }
+    });
+
+    window.addEventListener("locationchange", function () {
+        executeActions();
+    });
+
+    executeActions();
+
+    let id = setInterval(executeActions, 500);
+
+    setTimeout(() => { clearInterval(id); }, 2000);
+
+    function executeActions() {
+        const parsedUrl = new URL(location.href);
+        if (!parsedUrl.pathname.includes("shorts") || parsedUrl.pathname.endsWith("shorts")) {
+            return;
+        }
+
+        const videoId = parsedUrl.pathname.split("/").pop();
+        const s = `${parsedUrl.origin}/watch?v=${videoId}`;
+        if (s && location.href !== s) {
+            oldReplaceState.apply(history, [{}, '', s]);
+            location.replace(s);
         }
     }
 })();
